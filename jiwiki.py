4a36fa01bc25ace82b6da5c5e59743db413209f7
ricardo <thespirit@vip.qq.com>
2021-12-05 23:50:07 +0800

    init

---
+++ jiwiki.py
@@ -0,0 +1,56 @@
+from hoshino import Service, aiorequests, logger
+from nonebot.message import CQEvent
+
+sv = Service('小鸡词典')
+help_txt = """
+小鸡词典使用指北
+xxx是什么梗 查询xxx的含义
+""".strip()
+host = 'https://api.jikipedia.com/go/search_entities'
+header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36',
+    'content-type': 'application/json'
+}
+
+
+@sv.on_fullmatch(('小鸡词典', '小鸡字典'))
+async def help(bot, event: CQEvent):
+    await bot.send(event, help_txt)
+
+
+@sv.on_suffix(('是什么梗'))
+async def query(bot, event: CQEvent):
+    keyword = event.message.extract_plain_text().strip()
+    if not keyword:
+        return
+    request_data = {'page': 1, 'phrase': keyword, 'size': 60}
+    try:
+        resp = await aiorequests.post(host, headers=header, json=request_data, timeout=10)
+        res = await resp.json()
+    except Exception as ex:
+        logger.exception(ex)
+        await bot.finish(event, '查询错误，请重试')
+
+    data = res['data']
+    if len(data) == 0:
+        await bot.finish(event, f'没有查询到关于{keyword}的结果')
+
+    result_txt = ''
+    definitions = {}
+    for el in data:
+        if el['category'] != 'definition':
+            continue
+        if len(el['definitions']) == 0:
+            continue
+        definitions.update(el['definitions'][0])
+        break
+
+    if len(definitions) == 0:
+        await bot.finish(event, f'没有查询到关于{keyword}的结果')
+
+    term = definitions['term']
+    title = term['title']
+    if title != keyword:
+        result_txt = f'没有找到{keyword}，我猜你可能在找{title}\n'
+    result_txt += definitions['plaintext']
+    await bot.finish(event, result_txt)
