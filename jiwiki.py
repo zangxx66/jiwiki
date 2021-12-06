dc4e8dbdd21b54446c34800a19198563cdcf0ceb
ricardo <thespirit@vip.qq.com>
2021-12-06 18:23:05 +0800

    Add request restrictions,fix#1

--- jiwiki.py
+++ jiwiki.py
@@ -1,4 +1,5 @@
 from hoshino import Service, aiorequests, logger
+from hoshino.util import FreqLimiter
 from nonebot.message import CQEvent
 
 sv = Service('小鸡词典')
@@ -11,6 +12,7 @@ header = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36',
     'content-type': 'application/json'
 }
+freq = FreqLimiter(60)
 
 
 @sv.on_fullmatch(('小鸡词典', '小鸡字典'))
@@ -24,12 +26,22 @@ async def query(bot, event: CQEvent):
     if not keyword:
         return
     request_data = {'page': 1, 'phrase': keyword, 'size': 60}
+    gid = event.group_id
+    if not freq.check(gid):
+        await bot.finish(event, f'冷却时间中，请{int(freq.left_time(gid)) + 1}秒后再来')
     try:
         resp = await aiorequests.post(host, headers=header, json=request_data, timeout=10)
         res = await resp.json()
     except Exception as ex:
         logger.exception(ex)
         await bot.finish(event, '查询错误，请重试')
+
+    if 'data' not in res:
+        msg = res['message']
+        content = msg['content']
+        await bot.finish(event, content)
+
+    freq.start_cd(gid)
 
     data = res['data']
     if len(data) == 0:
